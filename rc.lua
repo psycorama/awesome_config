bf2cfc3c8883c65d3aa9bcf22a442b8abbf12a3b
Andreas Geisenhainer <psycorama@datenhalde.de>
2017-12-11 21:07:37 +0100

    rearranges layout sequence to start with maximesed layout

--- rc.lua
+++ rc.lua
@@ -83,21 +83,19 @@ config = {}
 -- Table of layouts to cover with awful.layout.inc, order matters.
 config.layouts =
 {
---    awful.layout.suit.tile,
---    awful.layout.suit.tile.left,
---    awful.layout.suit.tile.bottom,
---    awful.layout.suit.tile.top,
+    awful.layout.suit.max,
     awful.layout.suit.fair,
     awful.layout.suit.fair.horizontal,
+    awful.layout.suit.floating,
+    awful.layout.suit.tile,
+    awful.layout.suit.tile.left,
+    awful.layout.suit.tile.bottom,
+    awful.layout.suit.tile.top,
 --    awful.layout.suit.spiral,
 --    awful.layout.suit.spiral.dwindle,
-    awful.layout.suit.max,
 --    awful.layout.suit.max.fullscreen,
-    awful.layout.suit.floating,
 --    awful.layout.suit.magnifier
 }
---
-
 
 -- ########## loading a bunch of stuff #############
 loadrc("tags")
